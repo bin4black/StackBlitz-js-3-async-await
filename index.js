274f962574c2619be011839d39a0f56ef51aaaba
bin4black <122944796+bin4black@users.noreply.github.com>
2023-01-29 08:37:51 +0100

    1 file modified

--- index.js
+++ index.js
@@ -5,3 +5,27 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 console.log ("mi primer async-await")
+
+let stocks = {
+  Fruits: ['strawberry', 'grapes', 'banana', 'apple'],
+  liquid: ['water', 'ice'],
+  holder: ['cone', 'cup', 'stick'],
+  toppings: ['chocolate', 'peanuts'],
+};
+let is_shop_open = true;
+
+async function order(){
+  try{
+    await abc;
+  }
+  catch(error){
+    console.log("abc no existe",error)
+
+  }
+
+  finally{
+    console.log("runs anyway")
+  }
+}
+
+order()
